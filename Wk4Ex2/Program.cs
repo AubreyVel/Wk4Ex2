66e0521a9bbe70d6dc14675879cf1fbed0482be5
AubreyVel <agvelasco@amarillocollege.com>
2025-04-16 16:39:16 -0500

    Add project files.

---
+++ Wk4Ex2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk4Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declarations
+
+
+
+        }
+
+        public static double AssignmentsGrade()
+        {
+
+        }
+        public static double MidtermsGrade()
+        {
+
+        }
+        public static double FinalExamsGrade()
+        {
+
+        }
+        public static double CalculateFinalGrade()
+        {
+
+        }
+    }
+}
